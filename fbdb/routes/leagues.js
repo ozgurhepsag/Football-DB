d7d025a55504eb736a12a5c9d157228e60dc3969
Özgür Hepsağ <ozgurhepsag@gmail.com>
2018-12-16 21:26:24 +0300

    List Teams of the desired League

--- fbdb/routes/leagues.js
+++ fbdb/routes/leagues.js
@@ -9,8 +9,35 @@ router.get('/', function(req, res, next){
     });
 });
 
-/* GET leagues listing. */
-router.get('/list', function(req, res, next){
+router.get('/:id/teams', function(req, res, next){
+    res.render('league_teams', {
+        title: 'Teams',
+    });
+});
+
+/*GET teams of the desired league*/
+router.get('/:id/teams/list', function(req, res, next){
+    console.log(req.params.id);
+    var sql = `
+    SELECT idTeam as t_id, name as t_name, foundationYear as t_year,
+        stadium as t_stadium, logo as t_logo, color1 as t_color1, 
+        color2 as t_color2, league as l_id
+    FROM fbdb.team
+    WHERE league = ?; 
+    `;
+
+    db.query(sql, [req.params.id], function(error, result){
+        if (error) {
+            res.status(404).json({
+                error: "Failed to get teams."
+            });
+        }
+        res.status(200).json(result);
+    });
+});
+
+/* GET leagues list. */
+router.get('/list', function(req, res, next){ // league logosu eklenebilir
     var sql = `
     SELECT L.idLeague as l_id, L.name as l_name, C.flag as c_flag 
     FROM fbdb.league as L, fbdb.country as C 
