69cb6d7bc3ba29c2f2f7939d264280ba4d8c8eff
Özgür Hepsağ <ozgurhepsag@gmail.com>
2018-12-18 22:42:12 +0300

    Added player list and club profile (Player images and club images need to be fixed)

---
+++ fbdb/routes/players.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../lib/db');
+
+//app.js'e router'ı eklenmedi
+
+module.exports = router;
--- fbdb/routes/teams.js
+++ fbdb/routes/teams.js
@@ -1,10 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
 
-/* GET teams listing. */
-router.get('/', function(req, res, next){
-    res.render('team', {
-        title: 'Teams',
+router.get('/:id', function(req, res, next){
+
+    var sql = `
+    SELECT name as t_name FROM fbdb.team where idTeam = ?;
+    `;
+
+    db.query(sql, [req.params.id], function(error, result){
+        if (error) {
+            res.status(404).json({
+                error: "Failed to get leagues."
+            });
+        }
+        
+        res.render('team', {
+            title: result[0].t_name,
+        });
+    });
+});
+
+//SELECT * FROM fbdb.team where idTeam = 1;
+
+router.get('/:id/profile', function(req, res, next){
+
+    var sql = `
+    SELECT idTeam as t_id, name as t_name, foundationYear as t_fy, stadium as t_stadium, logo as t_logo 
+    FROM fbdb.team 
+    WHERE idTeam = ?;
+    `;
+
+    db.query(sql, [req.params.id], function(error, result){
+        if (error) {
+            res.status(404).json({
+                error: "Failed to get team profile."
+            });
+        }
+        
+        res.status(200).json(result[0]);
+    });
+});
+
+router.get('/:id/players', function(req, res, next){
+
+    var sql = `
+    SELECT idPlayer as p_id, firstName as p_fname, lastName as p_lname, position as p_pos, birthDate as p_birthdate, image as p_image, number as p_number 
+    FROM fbdb.player, fbdb.player_team 
+    WHERE team = ? and fbdb.player.idPlayer = fbdb.player_team.player;
+    `;
+
+    db.query(sql, [req.params.id], function(error, result){
+        if (error) {
+            res.status(404).json({
+                error: "Failed to get players."
+            });
+        }
+        
+        res.status(200).json(result);
     });
 });
 
